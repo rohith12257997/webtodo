c14020abeb7e2afae6d64a6329715cfcd0f89b1f
rohith12257997 <A00245384@mycambrian.ca>
2022-04-08 22:02:05 -0400

    todoapp

---
+++ ToDoList/Controllers/TodoController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Infastructure;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TodoController : Controller
+    {
+
+        private readonly TodoContext context;
+
+        public TodoController(TodoContext context)
+        {
+            this.context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<TodoList> items = from i in context.TodoList orderby i.Id select i;
+
+            List<TodoList> todoList = await items.ToListAsync();
+
+            return View(todoList);
+        }
+
+        public IActionResult Create() => View();
+
+     [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "This to-do item has been added";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        
+        public async Task<ActionResult> Edit(int id)
+        {
+            TodoList item = await context.TodoList.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(TodoList item)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Update(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "This to-do item has been updated";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        
+        public async Task<ActionResult> Delete(int id)
+        {
+            TodoList item = await context.TodoList.FindAsync(id);
+
+            if (item == null)
+            {
+                TempData["Error"] = "This to-do item does not exist";
+            }
+            else
+            {
+                context.TodoList.Remove(item);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "This to-do item has been Deleted";
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
